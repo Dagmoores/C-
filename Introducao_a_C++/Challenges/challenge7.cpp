3b85dc7187d4f62ed93afe9e88387ab008f7b472
Daniel Moore <dagmoores@gmail.com>
2021-03-10 18:32:13 -0300

    Update challenge7.cpp

--- Introducao_a_C++/Challenges/challenge7.cpp
+++ Introducao_a_C++/Challenges/challenge7.cpp
@@ -8,23 +8,35 @@ o algoritmo mostra a soma entre estas duas matrizes */
 
 using namespace std;
 
-int i;
-int j;
+int x;
+int y;
+int i2;
+int j2;
 
 int main() {
 
-  cout << "Type the number of rows in your matrix: ";
-  cin >> i;
-  cout << endl;
-
-  cout << "Now, type the number of columns in your matrix: ";
-  cin >> j;
-  cout << endl;
-
-  cout << "The dimensions of your matrix are " << i << " by " << j
-    << endl;
+  x = 2;
+  y = 3;
   
-  int matrix[i][j];
+  int matrix1[x][y];
+  matrix1[0][0] = 1;
+  matrix1[0][1] = 2;
+  matrix1[0][2] = 3;
+  matrix1[1][0] = 4;
+  matrix1[1][1] = 5;
+  matrix1[1][2] = 6;
+  
+  cout << "The dimension of matrix One are " << x << " by " << y
+    << endl;
+  cout << "The matrix One is: " << endl;
+
+  for(int i = 0; i < 2; i++) {
+    for (int j = 0; j < 3; j++) {
+      cout << matrix1[i][j] << " ";
+    }
+    cout << endl;
+  }
+
   
   return 0;
 }
