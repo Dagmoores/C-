2ed9d68d8c8032ba599e5a71b813dc07efe207a1
Daniel Moore <dagmoores@gmail.com>
2021-03-10 17:38:16 -0300

    Create challenge6.cpp

---
+++ Introducao_a_C++/Challenges/challenge6.cpp
@@ -0,0 +1,28 @@
+//Chalenge 6
+
+#include <iostream>
+
+using namespace std;
+
+int familyMoore;
+int familySilva;
+int familySauro;
+int familyLainetti;
+int familyLeone;
+
+int main() {
+
+    familyMoore = 55;
+    familySilva = 34;
+    familySauro = 69;
+    familyLainetti = 54;
+    familyLeone = 100;
+
+   int Media = (familyMoore + familySilva + familySauro + familyLainetti 
+  + familyLeone) / 5;
+
+   cout << "Media: " << Media << endl;
+
+  
+    return 0;
+}
