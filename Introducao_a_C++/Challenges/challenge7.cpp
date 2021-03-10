c031cc68fa4767ac4f31d5156bc265a92c458e2d
Daniel Moore <dagmoores@gmail.com>
2021-03-10 18:09:41 -0300

    Create challenge7.cpp

---
+++ Introducao_a_C++/Challenges/challenge7.cpp
@@ -0,0 +1,19 @@
+//Challenge 7
+
+/* Crie um algoritmo em C++, em que o usuário informa a dimensão de uma
+matriz, e os elementos de duas matrizes (ambas com a mesma dimensão) e
+o algoritmo mostra a soma entre estas duas matrizes */
+
+#include <iostream>
+
+using namespace std;
+
+int i;
+int j;
+
+int main() {
+
+  int matrix[i][j];
+  
+  return 0;
+}
