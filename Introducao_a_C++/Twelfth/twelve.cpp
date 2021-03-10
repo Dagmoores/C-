a3a77e11d07666dea5a934cc543faa1a2d4a2a73
Daniel Moore <dagmoores@gmail.com>
2021-03-10 10:28:26 -0300

    Update twelve.cpp

--- Introducao_a_C++/Twelfth/twelve.cpp
+++ Introducao_a_C++/Twelfth/twelve.cpp
@@ -1,4 +1,4 @@
-  
+ 
 #include <iostream>
 
 using namespace std;
@@ -24,8 +24,10 @@ int main() {
     int vectorSize;
     cout << "Type the vector size: " <<endl;
     cin >> vectorSize;
-    int* vector = new int[vectorSize];
-    
+    int* vector = new int[vectorSize];  
+    /* here we created the vector, and instantiate a pointer to
+     have access it.
+    */
     for (int i = 0; i < vectorSize; i++) {
         cout << "Type the element "<< i+1 << "of the vector" << endl;
         cin >> vector[i];
@@ -35,6 +37,8 @@ int main() {
      cout << vector[i] << " "; 
    }
    cout << endl;
+
+  delete [] vector //this way, with [] we can delete the vector.
   
     return 0;
 }
