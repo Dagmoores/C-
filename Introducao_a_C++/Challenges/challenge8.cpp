60384c9a69b80a53d954915fde2bf81c2f236e81
Daniel Moore <dagmoores@gmail.com>
2021-03-11 15:47:37 -0300

    Create challenge8.cpp

---
+++ Introducao_a_C++/Challenges/challenge8.cpp
@@ -0,0 +1,25 @@
+//Challenge 8
+/* Crie um algoritmo em C++, que o usuário informa o tamanho
+de um vetor, e seus elementos. O algoritmo deve imprimir este vetor
+e logo após, perguntar ao usuário se ele quer imprimir outro vetor.
+Utilize o delete após a impressão do vetor. */
+
+#include <iostream>
+
+using namespace std;
+
+int i;
+
+int main() {
+
+    int vector[i]; 
+  
+    cout << "Type the vector size: ";
+      cin >> i;
+
+  
+    cout << "Type the first element of this vector: " 
+      cin >> vector[0]
+  
+    return 0;
+}
