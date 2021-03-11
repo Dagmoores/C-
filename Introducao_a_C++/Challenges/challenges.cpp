ff930c2d38dadeb2e31bb102d55947ca4baaceb7
Daniel Moore <dagmoores@gmail.com>
2021-03-11 19:19:54 -0300

    Create challenges.cpp

---
+++ Introducao_a_C++/Challenges/challenges.cpp
@@ -0,0 +1,14 @@
+//Challenge9
+
+/* Crie um Algoritimo em C++ utilizando apenas ponteiros, em que o
+usuário informa a idade de duas pessoas, e o Algoritmo informa a 
+média destas duas idades. */
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+   
+  return 0;
+}
