d08f2cef2f14280a23cf5bf332c23a754291312d
Daniel Moore <dagmoores@gmail.com>
2021-03-11 22:11:45 -0300

    Update challenge9.cpp

--- Introducao_a_C++/Challenges/challenge9.cpp
+++ Introducao_a_C++/Challenges/challenge9.cpp
@@ -9,6 +9,28 @@ média destas duas idades. */
 using namespace std;
 
 int main () {
-   
+    
+    int age1;
+    int age2;
+    int* pointerAge1;
+    int* pointerAge2;
+
+    //Age 1 request
+    cout << "Enter the age of person 1: ";
+    cin >> age1;
+    cout << endl;
+
+    //Age 2 request
+    cout << "Enter the age of person 2: ";
+    cin >> age2;
+    cout << endl;
+
+    pointerAge1 = &age1;
+    pointerAge2 = &age2;
+
+    cout << "The media between the age 1 and the age 2 is: ";
+    cout << (*pointerAge1 + *pointerAge2) / 2;
+    cout << endl;
+
   return 0;
 }
